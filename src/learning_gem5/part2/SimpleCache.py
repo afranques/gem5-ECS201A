9d78caf9c80736007cade2b401c6ffdb33d06f3e
Jason Lowe-Power <jason@lowepower.com>
2017-10-06 14:54:02 -0700

    learning_gem5: Adding code for SimpleCache
    This is the rest of the code for part 2.
    
    See http://learning.gem5.org/book/part2/simplecache.html
    
    Change-Id: I5db099266a1196914656be3858fdd5fb4f8eab48
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ src/learning_gem5/part2/SimpleCache.py
@@ -0,0 +1,18 @@
+from m5.params import *
+from m5.proxy import *
+from MemObject import MemObject
+
+class SimpleCache(MemObject):
+    type = 'SimpleCache'
+    cxx_header = "learning_gem5/part2/simple_cache.hh"
+
+    # Vector port example. Both the instruction and data ports connect to this
+    # port which is automatically split out into two ports.
+    cpu_side = VectorSlavePort("CPU side port, receives requests")
+    mem_side = MasterPort("Memory side port, sends requests")
+
+    latency = Param.Cycles(1, "Cycles taken on a hit or to resolve a miss")
+
+    size = Param.MemorySize('16kB', "The size of the cache")
+
+    system = Param.System(Parent.any, "The system this cache is part of")
